0a45c7ca8d623c1f321aa26e26db9dba17b9a3bf
Abel Diress <abeldiress05@gmail.com>
2023-02-18 11:51:31 -0500

    START: added initial file structure

---
+++ server/src/models/Teacher.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Teacher = mongoose.Schema({
+    teacher_number: {
+        type: String,
+        required: true,
+    },
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    pfp: {
+        type: String,
+        required: true,
+    },
+    school: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    classes: [String],
+    deadlines: [String]
+});
+
+module.exports = mongoose.model('teacher', Teacher);
