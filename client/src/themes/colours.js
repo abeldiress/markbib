ca30b4473b28cd21956908f810dbbe3e9984e8d6
Abel Diress <abeldiress05@gmail.com>
2023-02-28 12:26:14 -0500

    added themes and colours from figma design

---
+++ client/src/themes/colours.js
@@ -0,0 +1,9 @@
+const colours = {
+  primary: "#153451",
+  black: "#0D090A",
+  grey: "#E0ECF5",
+  white: "#FDFFFC",
+  secondary: "#0A73EB",
+};
+
+export default colours;
---
+++ client/src/themes/fonts.js
@@ -0,0 +1,51 @@
+const fonts = {
+  BigBold: {
+    fontSize: 30,
+    fontFamily: 'DMSans_700Bold',
+  },
+  BigMedium: {
+    fontSize: 30,
+    fontFamily: 'DMSans_500Medium',
+  },
+  BigRegular: {
+    fontSize: 30,
+    fontFamily: 'DMSans_400Regular',
+  },
+  MedBold: {
+    fontSize: 16,
+    fontFamily: 'DMSans_700Bold',
+  },
+  MedMedium: {
+    fontSize: 16,
+    fontFamily: 'DMSans_500Medium',
+  },
+  MedRegular: {
+    fontSize: 16,
+    fontFamily: 'DMSans_400Regular',
+  },
+  SmallBold: {
+    fontSize: 14,
+    fontFamily: 'DMSans_700Bold',
+  },
+  SmallMedium: {
+    fontSize: 14,
+    fontFamily: 'DMSans_500Medium',
+  },
+  SmallRegular: {
+    fontSize: 14,
+    fontFamily: 'DMSans_400Regular',
+  },
+  TinyBold: {
+    fontSize: 10,
+    fontFamily: 'DMSans_700Bold',
+  },
+  TinyMedium: {
+    fontSize: 10,
+    fontFamily: 'DMSans_500Medium',
+  },
+  TinyRegular: {
+    fontSize: 10,
+    fontFamily: 'DMSans_400Regular',
+  },
+};
+
