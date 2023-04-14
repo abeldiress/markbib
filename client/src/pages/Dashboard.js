772ff9561c22f716a15a6052c35c5a14509ddff7
Methira Herath <methira.19@gmail.com>
2023-04-14 13:36:27 -0400

    Dashboard page template

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,19 +1,86 @@
-import SmallAssignmentList from "../molecules/SmallAssignmentList";
-import ClassList from "../molecules/ClassList";
-import useFetch from "../useFetch";
-import RefLink from "../atoms/RefLink";
-
 const Dashboard = () => {
-  const {data: Classes} = useFetch('http://localhost:3001/teacher/{uuid}')
-  const {data: Assignments} = useFetch('http://localhost:3001/Assignments')
-
+  // const {data: Classes} = useFetch('http://localhost:5000/teacher/{uuid}')
+  // const {data: Assignments} = useFetch('http://localhost:5000/Assignments')
+  const pagedata = {
+    Classes: [
+      {
+        id:1, 
+        name:"chem", 
+        ib:true,
+        list_assignments: [1, 2, 3],
+        list_students: [123456789, 234567891, 123456780],
+        meow: 7534987598347598
+      },
+      {
+        id:2,
+        name:"physics",
+        ib:true,
+        list_assignments: [4],
+        list_students: [123456789, 234567891, 123456780],
+        meow: 123123123
+      }
+    ],
+    Assignments: [
+      {
+        id:1,
+        name:"chem lab",
+        scores:0
+      },
+      {
+        id:2,
+        name:"organic chem test",
+        scores:2
+      },
+      {
+        id:3, 
+        name:"group 3 reflection",
+        scores:3
+      },
+      {
+        id:4, 
+        name:"group 3 reflection",
+        scores:3
+      }
+    ]
+  }
   return (
-    <div className="dashboard">
-      { Assignments && <SmallAssignmentList Assignments={Assignments} header="Assignments"/> }
-      <RefLink path="/assignments" text="View All"/>
-      { Classes && <ClassList Classes={Classes} header="Classes"/>}
-      <RefLink path="/classes/edit" text="Edit"/>
-    </div>
+    // <div>
+    //   {pagedata.Classes.map((Class) => (
+    //     <div key={Class.id}>
+    //       {pagedata.Assignments.map((Assignment) => (
+    //         <div key={Assignment.id}>
+    //           <h2>{Assignment.name}</h2>
+    //             {Class.list_assignments.includes(Assignment.id) && <h3>{Class.name}</h3>}
+    //         </div>
+    //         ))}
+    //     </div>
+    //   ))}
+    // </div>
+
+    <div>
+      <h1> Assignments </h1>
+      {pagedata.Assignments.map((Assignment) => (
+
+        <div key={Assignment.id}>
+          <h2>{Assignment.name}</h2>
+          {pagedata.Classes.map((Class) => (
+            <div key={Class.meow}>
+              {Class.list_assignments.includes(Assignment.id) && <h3>{Class.name}</h3>}
+              <h3> {Assignment.scores} / {Class.list_students.length} rubrics </h3>
+            </div>
+          ))}
+        </div>
+      ))}
+      <h1> Classes </h1>
+      {pagedata.Classes.map((Class) => (
+        <div key={Class.id}>
+          <h2> { Class.name } </h2>
+          <h3> { Class.list_assignments.length } assignments </h3>
+          <h3> { Class.list_students.length} students </h3>
+        </div>
+      ))}
+    </div> 
+
   );
 }
  
