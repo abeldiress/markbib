13d686047ff8b8bb4fdb183d61ac77fbdec831d6
Bilal Shaikh <bshai1@ocdsb.ca>
2023-04-26 13:27:15 -0400

    Fixed validation schemas and implemented into cont

--- server/src/controllers/expectation.controller.js
+++ server/src/controllers/expectation.controller.js
@@ -3,7 +3,16 @@ const { createValidation } = require('../utils/validation/expectation.validation
 const { updateValidation } = require('../utils/validation/expectation.validation');
 
 const createExpectation = async (req, res) => {
-    // validating create req
+    /**
+     * creates a new expectation
+     * @param {string} req.body.name - name of expectation
+     * @param {string} req.body.description - description of expectation
+     * @param {string} req.body.category - category of expectation
+     * @param {string} req.body.level - level of expectation
+     * @param {string} req.body.teacher - teacher of expectation
+     * @returns {object} - expectation object
+     * @returns {string} - error message
+     */
     const { error } = createValidation(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
     const expectation = new Expectation(req.body);
@@ -18,6 +27,12 @@ const createExpectation = async (req, res) => {
 
 
 const deleteExpectation = async(req, res) => {
+    /**
+     * deletes an expectation
+     * @param{string} req.body.expectation_id - expectation id
+     * @returns {object} - expectation object
+     * @returns {string} - error message
+     */
     const expectation = await Expectation.findOne({ _id: req.body.expectation_id });
     if (!expectation) return res.status(400).json({ error: 'Expectation not found.' });
     try {
@@ -30,7 +45,13 @@ const deleteExpectation = async(req, res) => {
 }
 
 const updateExpectation = async (req, res) => {
-    // validating update rq
+    /**
+     * updates an expectation
+     * @param {string} req.body.expectation_id - id of expectation
+     * @param {string} req.body - properties of expectation to update
+     * @returns {object} - expectation object
+     * @returns {string} - error message
+     */
     const {error} = updateValidation(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
 
--- server/src/controllers/student.controller.js
+++ server/src/controllers/student.controller.js
@@ -4,9 +4,16 @@ const { updateValidation } = require("../utils/validation/student.validation")
 
 
 const createStudent = async (req, res) => {
+    /**
+     * creates a new student 
+        * @param {string} req.body.name - name of student
+        * @param {string} req.body.classroom - classroom of student
+        * @param {string} req.body.teacher - teacher of student
+        * @returns {object} - student object
+        * @returns {string} - error message
+     */
     const {error} = createValidation(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
-
     const student = new Student(req.body);
     try {
         const savedStudent = await student.save();
@@ -17,16 +24,29 @@ const createStudent = async (req, res) => {
 }
 
 const getStudent = async (req, res) => {
-    const student = await Student.findOne({ _id: req.body.student_id });
+    /**
+     * gets a student
+     * @param {string} req.body._id - id of student
+     * @returns {object} - student object
+     * @returns {string} - error message
+     * 
+    */
+    const student = await Student.findOne({ _id: req.body._id });
     if (!student) return res.status(400).json({ error: 'Student not found.' });
     return res.json(student);
 }
 
 const deleteStudent = async (req, res) => {
-    const student = await Student.findOne({ _id: req.body.student_id });
+    /**
+     * deletes a student
+     * @param {string} req.body._id - id of student
+     * @returns {object} - student object
+     * @returns {string} - error message
+    */
+    const student = await Student.findOne({ _id: req.body._id });
     if (!student) return res.status(400).json({ error: 'Student not found.' });
     try {
-        const deletedStudent = await Student.deleteOne({ _id: req.body.student_id });
+        const deletedStudent = await Student.deleteOne({ _id: req.body._id });
         return res.json(deletedStudent);
     } catch (err) {
         return res.json(err);
@@ -34,19 +54,25 @@ const deleteStudent = async (req, res) => {
 }
 
 const updateStudent = async (req, res) => {
+    /**
+     * updates a student
+     * @param {string} req.body._id - id of student
+     * @param {string} req.body - properties to update
+     * @returns {object} - student object
+     * @returns {string} - error message
+    */
     const {error} = updateValidation(req.body);
     if (error) return res.status(400).json({ error: error.details[0].message });
 
-    const student = await Student.findOne({ _id: req.body.student_id });
+    const student = await Student.findOne({ _id: req.body._id });
     if (!student) return res.status(400).json({ error: 'Student not found.' });
     
     for(const property in req.body){
-        if (property == '_id') {
+        if (property == '_id' && req.body[property] != student._id) {
             return res.status(400).json({ error: 'Cannot change id of student!' });
         }
         student[property] = req.body[property];
     }
-    
     try {
         const savedStudent = await student.save();
         return res.json(savedStudent);
